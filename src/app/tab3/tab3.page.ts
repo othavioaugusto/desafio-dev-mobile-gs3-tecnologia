74602e5cec303686ff04cb81a4b9feb66292273b
Othavio Augusto <othavio@gmail.com>
2022-02-10 17:06:51 -0300

    Ultima versao

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -10,10 +10,19 @@ import { Geolocation } from '@capacitor/geolocation';
 })
 export class Tab3Page implements OnInit, OnDestroy {
   map: Leaflet.Map;
+  blueIcon: {};
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    this.blueIcon = Leaflet.icon({
+      iconUrl: '../../assets/icon/marker-icon.png',
+      iconSize: [25, 41],
+    });
+
+  }
+
 
   async ionViewDidEnter() {
     const coordinates = await Geolocation.getCurrentPosition();
@@ -22,21 +31,44 @@ export class Tab3Page implements OnInit, OnDestroy {
   }
 
   setGeoLocation(latitude, longitude) {
-    console.log(`LAT: ${latitude} - LON: ${longitude}`);
-
     this.map = Leaflet.map('mapId').setView([latitude, longitude], 15);
 
     Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'edupala.com © Angular LeafLet',
     }).addTo(this.map);
 
-    Leaflet.marker([latitude, longitude]).addTo(this.map).bindPopup('Minha localização').openPopup();
+    Leaflet.marker([latitude, longitude], { icon: this.blueIcon }).addTo(this.map).bindPopup('Minha localização').openPopup();
 
   }
 
-  /** Remove map when we have multiple map object */
   ngOnDestroy() {
     this.map.remove();
+  }
+
+  plotarPontosMapa() {
+    const arrayLatsLongs = [{ lat: '-15.837067', long: '-48.019114' },
+                            { lat: '-15.838136', long: '-48.022155' },
+                            { lat: '-15.840076', long: '-48.023507' },
+                            { lat: '-15.841851', long: '-48.020943' },
+                            { lat: '-15.841903', long: '-48.027412' },
+                            { lat: '-15.836753', long: '-48.012950' },
+                            { lat: '-15.835979', long: '-48.016609' },
+                            { lat: '-15.834647', long: '-48.019753' },
+                            { lat: '-15.832397', long: '-48.013509' },
+                            { lat: '-15.829321', long: '-48.013788' },
+                            { lat: '-15.837176', long: '-48.023498' }];
+
+    const posicao = this.getRandomIntInclusive(0, arrayLatsLongs.length - 1);
+    const latitude = arrayLatsLongs[posicao].lat;
+    const longitude = arrayLatsLongs[posicao].long;
+
+    Leaflet.marker([latitude, longitude], { icon: this.blueIcon }).addTo(this.map).bindPopup(`Ponto aleatório: [${posicao}]`).openPopup();
+  }
+
+  getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
 }
